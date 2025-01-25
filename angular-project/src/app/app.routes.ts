df9ee2648c3d39c978f32aee979025a579906ae1
perppp <153451320+perppp@users.noreply.github.com>
2025-01-25 13:00:55 +0400

    Update app.routes.ts

--- angular-project/src/app/app.routes.ts
+++ angular-project/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { UserManagementComponent } from './components/user-management/user-management.component'; // Import
+// import { UserManagementComponent } from './components/user-management/user-management.component'; // Import
 import { ReportsComponent } from './components/reports/reports.component'; // Import
 import { AuthGuard } from './guard/auth.guard';
 
@@ -11,6 +11,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'user-management', component: UserManagementComponent, canActivate: [AuthGuard] },
+  // { path: 'user-management', component: UserManagementComponent, canActivate: [AuthGuard] },
   { path: 'reports', component: ReportsComponent, canActivate: [AuthGuard] },
 ];
